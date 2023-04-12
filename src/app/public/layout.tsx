e84faf58677672bf2a63e6347d01b06fca36492d
Ryan Mehta <ryan.mehta@gmail.com>
2023-04-12 14:24:53 -0700

    fix container

--- src/app/public/layout.tsx
+++ src/app/public/layout.tsx
@@ -20,15 +20,11 @@ export default function AdminLayout({
 }) {
   return (
     <>
-      <div className="container m-auto">
+      <div>
         <div>
           <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
-            <div className="flex flex-1 justify-between items-center gap-x-4 self-stretch lg:gap-x-6">
-              <img
-                className="h-8 w-auto"
-                src="/zksig-logo.png"
-                alt="Your Company"
-              />
+            <div className="container m-auto flex flex-1 justify-between items-center gap-x-4 self-stretch lg:gap-x-6">
+              <img className="h-8 w-auto" src="/zksig-logo.png" alt="zksig" />
               <div className="flex items-center gap-x-4 lg:gap-x-6">
                 {/* Separator */}
                 <div
@@ -72,7 +68,7 @@ export default function AdminLayout({
           </div>
 
           <main className="py-10">
-            <div className="px-4 sm:px-6 lg:px-8">
+            <div className="px-4 sm:px-6 lg:px-8 container m-auto">
               <Main>{children}</Main>
             </div>
           </main>
